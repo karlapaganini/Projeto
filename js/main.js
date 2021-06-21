b398b0dfc748760b8534de107b191408710a2db2
Karla Paganini <karla1301_9@hotmail.com>
2021-06-21 17:41:17 -0300

    comit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// let botao = document.querySelector('.menu-principal__btn');
+// let menuPrincipal = document.querySelector('.menu-principal');
+
+// botao.addEventListener("click", abreFechaMenu);
+
+// function abreFechaMenu(evento){
+    // menuPrincipal.classList.toggle('menu-principal--fechado');
+// }
+
+// jquery
+$(document).ready(function(){
+    $('.menu-principal__btn').click(function(){
+        $('.menu-principal').toggleClass('menu-principal--fechado');
+    });
+
+    $('.depoimentos__caixa').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+    });
+
+    $('.formulario').validate({
+        rules: {
+            nome: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: 'required'
+        },
+        messages: {
+            nome: 'Por favor preencha o campo Nome',
+            email:{
+                required: 'Por favor preencha o e-mail.',
+                email: 'Por favor preencha um e-mail válido.'
+        },
+        mensagem: 'Por favor preencha o campo Mensagem.'
+      }
+    });
+  });
